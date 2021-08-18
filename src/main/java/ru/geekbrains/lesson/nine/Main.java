f85be0469ddc264875e3e498dbf5644ab82b946a
TexMerph <loshakovpv@yandex.ru>
2021-08-18 22:12:04 +0300

    Lesson 9 commit 5

--- src/main/java/ru/geekbrains/lesson/nine/Main.java
+++ src/main/java/ru/geekbrains/lesson/nine/Main.java
@@ -1,28 +1,29 @@
 package ru.geekbrains.lesson.nine;
 
 public class Main {
-    String i [][] = {
-        {"3", "5", "7", "9"},
-        {"2", "4", "6", "8"},
-        {"1", "7", "3", "5"},
-        {"0", "2", "8", "6"}
-    };
 
-    static void arrSize(String i [][]) {
+
+    static void arrSize(String[][] i) {
         if (i.length != 4) {
-            throw new MyArraySizeException(message);
+            throw new MyArraySizeException();
         }
     }
-    static int sumArr(String[][] nums) {
-        arrSize(nums.lenght);
-        for (int i = 0; i < val.length; i++) {
-            String[] strings = val[i];
+    static int sumArr(String[][] i) {
+        arrSize(i.length);
+        for (int i = 0; i < i.length; i++) {
+
 
         }
     }
 
     public static void main(String[] args) {
-        arrSize();
+        String[][] i  = {
+                {"3", "5", "7", "9"},
+                {"2", "4", "6", "8"},
+                {"1", "7", "3", "5"},
+                {"0", "2", "8", "6"}
+        };
+        arrSize(i);
     }
 
     public static class MyArraySizeException extends RuntimeException{
@@ -34,7 +35,7 @@ public class Main {
     public class MyArrayDataException extends RuntimeException{
         public MyArrayDataException(String message, Throwable cause) {
             super(message, cause);
-            message = "Array must be 4x4!";
+
         }
     }
 }
